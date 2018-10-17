df1c3fa5dbd245d968879b65bf7e0028fe76c716
SAKATA Sinji <sinji@sakasin.net>
2018-10-17 21:50:09 +0900

    Search 4x4 4x6 6x6

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,20 @@
 #include <chrono>
 #include <cstdint>
 #include <iostream>
+#include <list>
+#include <vector>
 using bitboard = uint64_t;
 
 enum class STATE {
   LOSE = -1,
   DRAW = 0,
   WIN = 1,
+};
+
+struct Board {
+  size_t depth;
+  bitboard dark;
+  bitboard light;
 };
 
 STATE rev_state(STATE status) {
@@ -25,7 +33,7 @@ std::ostream &operator<<(std::ostream &o, const STATE &state) {
   return o;
 }
 
-bitboard makeLegalBoard(bitboard dark, bitboard light) {
+bitboard makeCandidate(bitboard dark, bitboard light) {
   //左右端の番人
   bitboard horizontalWatchBoard = light & 0x7e7e7e7e7e7e7e7e;
   //上下端の番人
@@ -125,18 +133,6 @@ size_t popcnt(const bitboard &v) {
   return (count & 0x00000000ffffffff) + ((count >> 32) & 0x00000000ffffffff);
 }
 
-// must `~(dark | light) == 0`
-STATE juge_finished_board(bitboard dark) {
-  int score = popcnt(dark);
-  if (score > 8 * 8 / 2) {
-    return STATE::WIN;
-  } else if (score == 8 * 8 / 2) {
-    return STATE::DRAW;
-  } else {
-    return STATE::LOSE;
-  }
-}
-
 STATE juge(bitboard dark, bitboard light) {
   size_t dscore = popcnt(dark);
   size_t lscore = popcnt(light);
@@ -167,6 +163,56 @@ void print8x8(uint64_t dark, uint64_t light, bool rev = false) {
       dark = dark >> 1;
       light = light >> 1;
     }
+    std::cout << std::endl;
+  }
+  std::cout << "========" << std::endl;
+}
+
+void print6x6(uint64_t dark, uint64_t light, bool rev = false) {
+  if (rev) {
+    uint64_t tmp = dark;
+    dark = light;
+    light = tmp;
+  }
+  for (int i = 0; i < 6; i++) {
+    for (int p = 0; p < 6; p++) {
+      if (dark & 1UL) {
+        std::cout << "◯";
+      } else if (light & 1UL) {
+        std::cout << "●";
+      } else {
+        std::cout << ".";
+      }
+      dark = dark >> 1;
+      light = light >> 1;
+    }
+    dark = dark >> 2;
+    light = light >> 2;
+    std::cout << std::endl;
+  }
+  std::cout << "========" << std::endl;
+}
+
+void print4x4(uint64_t dark, uint64_t light, bool rev = false) {
+  if (rev) {
+    uint64_t tmp = dark;
+    dark = light;
+    light = tmp;
+  }
+  for (int i = 0; i < 4; i++) {
+    for (int p = 0; p < 4; p++) {
+      if (dark & 1UL) {
+        std::cout << "◯";
+      } else if (light & 1UL) {
+        std::cout << "●";
+      } else {
+        std::cout << ".";
+      }
+      dark = dark >> 1;
+      light = light >> 1;
+    }
+    dark = dark >> 4;
+    light = light >> 4;
     std::cout << std::endl;
   }
   std::cout << "========" << std::endl;
@@ -218,28 +264,29 @@ static uint64_t cnt_totyu = 0;
 static std::chrono::high_resolution_clock::time_point begin_at;
 static std::chrono::high_resolution_clock::time_point end_at;
 
+bitboard mask4x4 = 0x000000000f0f0f0f;
+bitboard mask4x6 = 0x000000003f3f3f3f;
+bitboard mask6x6 = 0x00003f3f3f3f3f3f;
+
 STATE negamax(bitboard dark, bitboard light, bool pass = false) {
   if (~(dark | light) == 0) {
     // 終局
     cnt++;
-    if (cnt >= 10000000ULL) {
-      end_at = std::chrono::high_resolution_clock::now();
-      size_t millisec = std::chrono::duration_cast<std::chrono::milliseconds>(
-                            end_at - begin_at)
-                            .count();
+    if (cnt % 10000000ULL == 0) {
       std::cout << cnt << std::endl;
-      std::cout << millisec << " ms" << std::endl;
-      exit(1);
     }
-    return juge_finished_board(dark);
+    return juge(dark, light);
   }
 
   STATE max = STATE::LOSE;
-  bitboard space = makeLegalBoard(dark, light);
+  bitboard space = makeCandidate(dark, light) & mask6x6;
   if (space == 0) {
     if (pass) {
       cnt_totyu++;
       cnt++;
+      if (cnt % 10000000ULL == 0) {
+        std::cout << cnt << std::endl;
+      }
       return juge(dark, light);
     }
 
@@ -262,26 +309,44 @@ STATE negamax(bitboard dark, bitboard light, bool pass = false) {
   return max;
 }
 
+static std::list<Board> board_queue;
+
+std::pair<bitboard, bitboard> initial_board_8x8 = {(1UL << 28) | (1UL << 35), (1UL << 27) | (1UL << 36)};
+std::pair<bitboard, bitboard> initial_board_6x6 = {(1UL << 19) | (1UL << 26), (1UL << 18) | (1UL << 27)};
+std::pair<bitboard, bitboard> initial_board_4x4 = {(1UL << 10) | (1UL << 17), (1UL << 9) | (1UL << 18)};
+std::pair<bitboard, bitboard> initial_board_4x6 = {(1UL << 11) | (1UL << 18), (1UL << 10) | (1UL << 19)};
+
 int main() {
+
+  // print8x8(initial_board_8x8.first, initial_board_8x8.second);
+  print6x6(initial_board_6x6.first, initial_board_6x6.second);
+  // print4x4(initial_board_4x4.first, initial_board_4x4.second);
+  // print8x8(initial_board_4x6.first, initial_board_4x6.second);
+
+  bitboard dark = initial_board_6x6.first;
+  bitboard light = initial_board_6x6.second;
+
+  bitboard candidate = makeCandidate(dark, light);
+  print8x8(candidate, 0);
+  print8x8(mask4x6, 0);
+
   // initial board
-  uint64_t dark = (1UL << 28) | (1UL << 35);
-  uint64_t light = (1UL << 27) | (1UL << 36);
-  // print8x8(dark, light);
+  // uint64_t dark = (1UL << 28) | (1UL << 35);
+  // uint64_t light = (1UL << 27) | (1UL << 36);
 
   { // first step
-    uint64_t space = makeLegalBoard(dark, light);
+    uint64_t space = makeCandidate(dark, light);
     // print8x8(space, 0);
     bitboard move = space & (~space + 1);
     bitboard rev = getRevPat(dark, light, move);
 
     dark = dark ^ (move | rev);
     light = light ^ rev;
-    // print8x8(dark, light);
+    print8x8(dark, light);
   }
 
-  begin_at = std::chrono::high_resolution_clock::now();
   std::cout << rev_state(negamax(light, dark)) << std::endl;
   std::cout << cnt << std::endl;
-  std::cout << cnt_totyu << std::endl;
+
   return 0;
 }
